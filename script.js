276ae5f981bb53f04f8fcc6d8289d01e9930cb0c
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-08-07 21:14:20 -0300

    Concluído requisito 7, finalizando o projeto meme generator

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let memeInput = document.getElementById('meme-insert');
 let memeImage = document.createElement('img')
 memeImage.id = 'meme-image'
 memeInput.addEventListener('change', function (evento) {
+    //O código utilizado para carregar o arquivo no meme-image-container foi refatorado conforme conteúdo deste site: https://stackoverflow.com/questions/33920409/how-to-get-local-filename-using-url-createobjecturl
     let src = URL.createObjectURL(evento.target.files[0]);
     memeImage.src = src;
     memeContainer.appendChild(memeImage);
@@ -27,15 +28,60 @@ let btnEarth = document.getElementById('earth');
 
 btnFire.addEventListener('click', function () {
     memeContainer.style.border = '3px dashed rgb(255, 0, 0)';    
-})
+});
 
 btnWater.addEventListener('click', function () {
     memeContainer.style.border = '5px double rgb(0, 0, 255)';    
-})
+});
 
 btnEarth.addEventListener('click', function () {
     memeContainer.style.border = '6px groove rgb(0, 128, 0)';    
-})
+});
+
+//Requisito 7 - Crie um conjunto de quatro imagens pré prontas que permita que a pessoa usuária possa escolher.
+let imgs = document.getElementById('imgs-container');
+
+//Com o código abaixo todas as imagens funcionam com apenas uma função
+// imgs.addEventListener('click', function (event) {
+//     let memePronto = event.target;
+//     let source = memePronto.src;
+//     memeImage.src = source;
+//     memeContainer.appendChild(memeImage);
+// });
+
+//As funções abaixo foram criadas porque o cypress não interpretou corretamente o source das imagens já carregadas no site
+let meme1 = document.getElementById('meme-1')
+
+meme1.addEventListener('click', function () {
+    // let source = memePronto.src;
+    memeImage.src = './imgs/meme1.png';
+    memeContainer.appendChild(memeImage);
+});
+
+let meme2 = document.getElementById('meme-2')
+
+meme2.addEventListener('click', function () {
+    // let source = memePronto.src;
+    memeImage.src = './imgs/meme2.png';
+    memeContainer.appendChild(memeImage);
+});
+
+let meme3 = document.getElementById('meme-3')
+
+meme3.addEventListener('click', function () {
+    // let source = memePronto.src;
+    memeImage.src = './imgs/meme3.png';
+    memeContainer.appendChild(memeImage);
+});
+
+let meme4 = document.getElementById('meme-4')
+
+meme4.addEventListener('click', function () {
+    // let source = memePronto.src;
+    memeImage.src = './imgs/meme4.png';
+    memeContainer.appendChild(memeImage);
+});
+
 
 
 
